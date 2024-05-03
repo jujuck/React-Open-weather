842dd2e61ab517a23e9003d58dc7c0770ba9c0d8
Julien RICHARD <julien.y.richard@gmail.com>
2024-05-03 14:28:01 +0200

    Gestion de l'entrée user sur la ville et mise en place du lien via un state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [city, setCity] = useState("s");
+
+  console.log(city);
   return (
     <main className="container">
       <h1>Mon application météo</h1>
+      <section>
+        <label htmlFor="">
+          Entrez votre ville
+          <input
+            type="text"
+            value={city}
+            onChange={(event) => setCity(event.target.value)}
+          />
+        </label>
+        <a href={`/${city}`}>Allez voir la météo de la ville de {city}</a>
+      </section>
     </main>
   );
 }
